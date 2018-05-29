2a77ce1df9b8ecec2c4625ed61ded8fedba7413b
ksneo <vkashapov@gmail.com>
2018-05-29 13:37:28 +0300

    Add scss to project

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('has h1 className App-title', () => {
+  const component = ReactTestUtils.renderIntoDocument(
+    <App />
+  );
+  const title = ReactTestUtils.findRenderedDOMComponentWithClass(component, 'App-title');
+  expect(title).toBeInstanceOf(HTMLHeadingElement);
 });
