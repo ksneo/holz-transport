a142be9fe45e7027aabbed6b6b76c103c9e6dd81
ksneo <vkashapov@gmail.com>
2018-06-06 18:10:37 +0300

    Add router

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,7 @@ import logo from 'assets/images/logo.png';
 import 'containers/App.css';
 import OrderInfoList from 'components/OrderInfoList';
 import { fetchOrders } from 'store/ordersData/actions';
+import { HashRouter, Route, Link } from 'react-router-dom';
 
 class App extends Component {
     componentDidMount() {
@@ -12,25 +13,26 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <div className="App-container">
-                    <header className="App-header">
-                        <img src={logo} className="App-logo" alt="logo"/>
-                        <h1 className="App-title">Транспорт</h1>
-                    </header>
-                    <main className='App-main'>
-                        <OrderInfoList orders={this.props.ordersData} />
-                    </main>
-                    <aside className='App-sidebar'>
-                        <ul>
-                            <li>Пункт меню</li>
-                            <li>Пункт меню</li>
-                            <li>Пункт меню</li>
-                            <li>Пункт меню</li>
-                        </ul>
-                    </aside>
+            <HashRouter>
+                <div className="App">
+                    <div className="App-container">
+                        <header className="App-header">
+                            <img src={logo} className="App-logo" alt="logo"/>
+                            <h1 className="App-title">Транспорт</h1>
+                        </header>
+                        <main className='App-main'>
+                            <Route path='/settings' render={() => <p>Здесь будут настройки</p>} />
+                            <Route exact path='/' render={() => <OrderInfoList orders={this.props.ordersData} />} />
+                        </main>
+                        <aside className='App-sidebar'>
+                            <ul>
+                                <li><Link to='/'>Заказы</Link></li>
+                                <li><Link to='/settings'>Настройки</Link></li>
+                            </ul>
+                        </aside>
+                    </div>
                 </div>
-            </div>
+            </HashRouter>
         );
     }
 }
